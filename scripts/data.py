4f2b2905c58e8d07b06dda6af905e67ac1ddcda1
Avare Stewart <a.stewart@eos-solutions.com>
2019-05-05 22:24:31 +0200

    refactorting remove dict function

--- scripts/data.py
+++ scripts/data.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from quilt.data.avare import homecredit
-
-# create an entity set
-es = ft.EntitySet(id="homecredit_data")
+import featuretools as ft
 
 
 def as_dict_featuretools(df):
@@ -13,13 +11,13 @@ def as_dict_featuretools(df):
     :return:
     '''
     # df has two columns: Row(column name) Type (a python dtype)
-    # categorical = 'object'
-    # numeric = 'float64'
+    categorical = 'object'
+    numeric = 'float64'
 
     ################ since it infers, i can leave the python types and not perform this replace as well
     # rename types
-    # df.replace(numeric, ft.variable_types.Numeric, inplace=True)
-    # df.replace(categorical, ft.variable_types.Categorical, inplace=True)
+    df.replace(numeric, ft.variable_types.Numeric, inplace=True)
+    df.replace(categorical, ft.variable_types.Categorical, inplace=True)
 
     # convert to dict
     tuples = dict([*zip(df.Row.values, df.Type.values)])
